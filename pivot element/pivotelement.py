c282a4859f37f8e902399f8ec1e87678fbf7b027
Aayusshman Baral <b.aayushman@op.iitg.ac.in>
2026-06-23 23:07:43 +0530

    pivotelement.py
    the solution for the pivot element finding in python

---
+++ pivot element/pivotelement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if sum(nums[0:i])==sum(nums[i+1:len(nums)]):
+                return i
+            
+        return -1
+            
